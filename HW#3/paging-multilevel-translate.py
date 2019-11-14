9766a7f12a7fb41d3fc8c1254a369db3a7117954
KidanJIN <ihansam@skku.edu>
2019-11-14 21:39:38 +0900

    implement test algorithm

--- HW#3/paging-multilevel-translate.py
+++ HW#3/paging-multilevel-translate.py
@@ -42,7 +42,7 @@ class OS:
         # os tracks
         self.usedPages      = []
         self.usedPagesCount = 0
-        self.maxPageCount   = self.physMem / self.pageSize
+        self.maxPageCount   = int(self.physMem / self.pageSize)
 
         # no pages used (yet)
         for i in range(0, self.maxPageCount):
@@ -187,7 +187,7 @@ class OS:
         print('')
 
     def memoryDump(self):
-        for i in range(0, self.physMem / self.pageSize):
+        for i in range(0, int(self.physMem / self.pageSize)):
             print('page %3d:' %  i, end=' ') 
             for j in range(0, self.pageSize):
                 print('%02x' % self.memory[(i * self.pageSize) + j], end=' ')
---
+++ HW#3/test.py
@@ -0,0 +1,40 @@
+# SKKU EEE 2015311951 Kidan Jin
+# OS intro HW#3
+# test implementation of clock algorithm
+  
+# variables
+ref = [1,2,3,4,1,2,5,1,2,3,4,5]     # page reference (testcase)
+mem = []                            # contents of memory
+use = []                            # use bit of memory
+size = 3                            # size of memory
+clkptr = 0                          # clock pointer
+
+# function
+def clockreplacement(cp, refnum):   # 현재 clock 포인터와 reference page number를 받아
+    replace_complete = False        # clock pointer를 갱신하고, swap out될 victim을 정하고, 메모리를 replacement하는 함수    
+    while not replace_complete:            
+        if use[cp] == 0:            # victime page found
+            victim = mem.pop(cp)        # replace
+            mem.insert(cp, refnum)
+            use[cp] = 1                 # set usebit
+            replace_complete = True
+        else:                       # give used page second chance
+            use[cp] = 0                 # reset use bit
+                                    # update clock pointer
+        cp = (lambda x: 0 if x == size-1 else x+1)(cp)      # 0->1->2->0...
+    return cp, victim
+
+# main
+for page in ref:
+    victim = " "                # reset victim
+    if page in mem:             # hit case
+        use[mem.index(page)] = 1    # set usebit
+        state = "hit "
+    elif len(mem) < size:       # cold miss case
+        mem.append(page)            # swap in
+        use.append(1)
+        state = "miss"
+    else:                       # miss case         
+        clkptr, victim = clockreplacement(clkptr, page)     # run replacement algorithm
+        state = "miss"
+    print("Reference #", page, "(ref)", state, "(usebit)", use, "(memory)", mem, "swap out:", victim, "clock pointer:", clkptr)
